bb31879378973961f252454e44cffc5ecc35693d
Matthieu <matthieu0405@hotmail.fr>
2023-12-27 19:16:33 +0100

    Changement Damier.hpp

--- include/Damier.hpp
+++ include/Damier.hpp
@@ -2,22 +2,23 @@
 #define DAMIER_HPP
 
 #include <vector>
+#include <string>
 using namespace std;
 
 class Damier{
-private: 
+protected: 
     int taille;
     /*Tableau de char à deux dimensions*/
-    vector<vector<char> > grille;
+    std::vector<vector<string>> grille;
 public:
     Damier(int taille);
     void afficher() const;
-    void setCellule(int x, int y, char valeur);
-    char getCellule(int x, int y) const;
+    void setCellule(int x, int y, const std::string& valeur);
+    string getCellule(int x, int y) const;
     int getTaille() const;
     bool CoordonneesValides(int x, int y) const;
-};
 
+};
 
 
 
